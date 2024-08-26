5949718d3b0fe5266ead0f1973b65b51cdb58a65
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-26 20:35:48 +0300

    Добавлена валидация

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -2,14 +2,22 @@ import express, { Request, Response } from 'express';
 
 import { IdNumberDto } from '../../shared/id.number.dto';
 import { validate } from '../../validation';
+import { CreateTaskDto } from './task.dto';
 
 const TaskController = {
   getOne(req: Request, res: Response) {
     const params = validate(IdNumberDto, req.params);
 
-    res.send(params);
+    res.json(params);
+  },
+
+  create(req: Request, res: Response) {
+    const body = validate(CreateTaskDto, req.body);
+
+    res.json(body);
   },
 };
 
 export const TaskRouter = express.Router();
 TaskRouter.get('/:id', TaskController.getOne);
+TaskRouter.post('/', TaskController.create);
