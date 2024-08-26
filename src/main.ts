c99fdc99e498cb4be63634c7a1fe5618a89d785b
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-08-26 20:19:36 +0300

    Добавлены роутеры

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,26 @@
-console.log(1);
-console.log(1);
+import express from 'express';
+
+import { logRoutes } from './log.routes';
+
+const server = express();
+server.use(express.json());
+
+const port = 2000;
+
+const taskRouter = express.Router();
+const userRouter = express.Router();
+
+taskRouter.get('/:id', (req, res) => {
+  res.send({ a: 10 });
+});
+
+userRouter.get('/:id', (req, res) => {});
+
+server.use('/task', taskRouter);
+server.use('/user', userRouter);
+
+logRoutes(server);
+
+server.listen(port, () => {
+  console.log(`Server is started on port ${port}...`);
+});
